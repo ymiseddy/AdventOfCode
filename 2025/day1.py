c426626165affc2eee3fce602de8ea18d8e4b490
Shawn Eddy <ymiseddy@gmail.com>
2025-12-01 01:28:38 -0500

    Day1 solution for AdventOfCode 2025.

---
+++ 2025/day1.py
@@ -0,0 +1,55 @@
+import tools as t
+import numpy as np
+from collections import Counter
+
+def load_data(filename):
+    data = t.load(filename)
+    lines = data.splitlines()
+    steps = [(x[0], int(x[1:])) for x in lines]
+    return steps
+
+def part_01(filename="2025/day1s1.txt"):
+    steps = load_data(filename)
+    p = 50
+    zcount = 0
+    maxr = 0
+    for (direction, count) in steps:
+        if count > maxr:
+            maxr = count
+        if direction == "R":
+            p = (p + count) % 100
+        else:
+            p = (p - count) % 100
+        if p == 0:
+            zcount += 1
+
+    print(f"Part 1: max= {maxr} zero count = {zcount}")
+
+def part_02(filename="2025/day1s1.txt"):
+    steps = load_data(filename)
+    p = 50
+    zcount = 0
+    for (direction, count) in steps:
+        ztimes = 0
+        if direction == "R":
+            d = 100 - p
+            ztimes = 1 + (count - d) // 100
+            p = (p + count) % 100
+        else:
+            d = p
+
+            # Don't count if we are starting on a zero
+            ztimes += 1 * (p != 0) + (count - d) // 100
+            p = (p - count) % 100
+        zcount += ztimes
+
+    print(f"Part 2: zero count = {zcount}")
+
+if __name__ == "__main__":
+    part_01("day1s1.txt")
+    part_01("day1.txt")
+
+    print()
+
+    part_02("day1s1.txt")
+    part_02("day1.txt")
---
+++ 2025/tools/__init__.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def load(name: str):
+    with open(name, "r") as f:
+        data = f.read()
+    return data
+
+def int_columns(data):
+    lines = data.splitlines()
+    # We want to split each line by spaces and convert to integers.
+    columns = []
+    for line in lines:
+        nums = [int(x) for x in line.split()]
+        for i, num in enumerate(nums):
+            if len(columns) <= i:
+                columns.append([])
+            columns[i].append(num)
+    # Convert to numpy arrays
+    for i in range(len(columns)):
+        columns[i] = np.array(columns[i])
+    return np.array(columns)
+
+
